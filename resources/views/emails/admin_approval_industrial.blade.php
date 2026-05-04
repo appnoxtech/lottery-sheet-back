924bf686d5d6768f44eeb6f9e698d57950c1cb6f
Kunal <kunal.appnox@gmail.com>
2026-05-04 12:48:22 +0530

    fix: mail template

--- resources/views/emails/admin_approval_industrial.blade.php
+++ resources/views/emails/admin_approval_industrial.blade.php
@@ -168,7 +168,7 @@
             </tr>
             <tr>
                 <td class="footer">
-                    <p>&copy; {{ date('Y') }} Admin Dashboard. All rights reserved.</p>
+                    <p>&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</p>
                 </td>
             </tr>
         </table>
--- resources/views/emails/password_reset_industrial.blade.php
+++ resources/views/emails/password_reset_industrial.blade.php
@@ -123,7 +123,7 @@
             </tr>
             <tr>
                 <td class="footer">
-                    <p>&copy; {{ date('Y') }} Admin Dashboard. All rights reserved.</p>
+                    <p>&copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</p>
                 </td>
             </tr>
         </table>
